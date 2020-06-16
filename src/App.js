6993b68e5c81cbee943d0773f31ca645c518811b
André Souza <als260602@msn.com>
2020-06-16 14:08:31 -0300

    Desafio - Conceitos do react

--- src/App.js
+++ src/App.js
@@ -1,29 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+
+    api.get('repositories')
+      .then(response => {
+        setRepositories(response.data)
+      })
+
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+
+    const url = 'https://github.com/als260502/chalenge-react-concepts'
+    const title = 'Desafio ReactJS'
+    const techs = ['ReactJS', 'Node.js', 'JavaScript']
+
+
+    const response = await api.post('repositories', {
+      title,
+      url,
+      techs
+    })
+
+    const newRepository = response.data
+
+    setRepositories([...repositories, newRepository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`repositories/${id}`)
+    const newRepositories = repositories.filter(repository => repository.id !== id)
+
+    setRepositories(newRepositories)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            <span>
+              <strong>
+                {repository.title}
+              </strong>
+              <a href={repository.url}>Link</a>
+              <strong>Tecnologia(as): {repository.techs.join(', ')}</strong>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            </span>
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
-      </ul>
+          </li>
+        ))}
 
+      </ul>
       <button onClick={handleAddRepository}>Adicionar</button>
+
     </div>
   );
 }
